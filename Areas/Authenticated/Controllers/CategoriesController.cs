80bac62fb2be5064675aafe9aaed7b35d7e4cea1
Dang Khoi <88148432+dankhoi@users.noreply.github.com>
2022-06-23 22:18:09 +0700

    Done import by excel

--- Areas/Authenticated/Controllers/CategoriesController.cs
+++ Areas/Authenticated/Controllers/CategoriesController.cs
@@ -14,20 +14,21 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml;
 
 namespace App_Dev_2.Areas.Authenticated.Controllers
 {
     [Area("Authenticated")]
-    [Authorize(Roles = SD.Role_Customer)]
+    [Authorize(Roles = SD.Role_StoreOwner)]
     public class CategoriesController : Controller
     {
-       
         private readonly ApplicationDbContext _db;
 
         public CategoriesController(ApplicationDbContext db)
         {
             _db = db;
         }
+
         // GET
         public IActionResult Index(string searchString)
         {
@@ -36,6 +37,7 @@ namespace App_Dev_2.Areas.Authenticated.Controllers
             {
                 listCategories = listCategories.Where(c => c.Name.Contains(searchString)).ToList();
             }
+
             return View(listCategories);
         }
 
@@ -48,7 +50,7 @@ namespace App_Dev_2.Areas.Authenticated.Controllers
             }
 
             var findCategory = _db.Categories.Find(id);
-    
+
             return View(findCategory);
         }
 
@@ -68,18 +70,42 @@ namespace App_Dev_2.Areas.Authenticated.Controllers
                 _db.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
+
             return View(category);
         }
 
-        // public async Task<List<IActionResult>> Import(IFormFile file)
-        // {
-        //     var list = new List<Category>();
-        //     using (var stream = new MemoryStream())
-        //     {
-        //         
-        //     }
-        //     return View();
-        // }
+        [HttpGet]
+        public IActionResult Import()
+        {
+            return View();
+        }
+        
+        
+        [HttpPost]
+        public async Task<IActionResult> Import(IFormFile file)
+        {
+            var list = new List<Category>();
+            using (var stream = new MemoryStream())
+            {
+                await file.CopyToAsync(stream);
+                using (var package = new ExcelPackage(stream))
+                {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets[1];
+                    var rowCount = worksheet.Dimension.Rows;
+                    for (int row = 2; row <= rowCount; row++)
+                    {
+                        list.Add(new Category()
+                        {
+                            Name = worksheet.Cells[row, 1].Value.ToString().Trim(),
+                            Description = worksheet.Cells[row, 2].Value.ToString().Trim()
+                        });
+                    }
+                }
+            }
+            _db.Categories.AddRange(list);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
 
         public IActionResult Delete(int id)
         {
@@ -88,10 +114,5 @@ namespace App_Dev_2.Areas.Authenticated.Controllers
             _db.SaveChanges();
             return RedirectToAction(nameof(Index));
         }
-        
     }
 }
-    
-
-        
-        
